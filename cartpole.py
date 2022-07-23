51689b2bf207ef3fe4d040ab7237e03eda0bfc7c
jnachyla <jarnach@agora.pl>
2022-07-23 18:13:50 +0200

    hackaton1

--- cartpole.py
+++ cartpole.py
@@ -11,7 +11,10 @@ env = gym.make('CartPole-v0').env
 env.seed(0)
 np.random.seed(0)
 
+'''
+The original code is of the base network: https://github.com/shivaverma/OpenAIGym/blob/master/cart-pole/CartPole-v0.py
 
+'''
 class DeepQLearningModel:
 
     """ Implementation of deep q learning algorithm """
@@ -25,7 +28,7 @@ class DeepQLearningModel:
         self.batch_size = 64
         self.epsilon_min = .01
         self.epsilon_decay = .995
-        self.lr = 0.001
+        self.lr = 0.0001
         self.memory_model = deque(maxlen=10000)
         self.model = self.build_model()
 
@@ -42,70 +45,68 @@ class DeepQLearningModel:
         print(model.summary())
         return model
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory_model.append((state, action, reward, next_state, done))
-
-    def act(self, state):
+    def get_action(self, state):
 
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_space)
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])
 
-    def replay(self):
+    def learn(self):
 
-        if len(self.memory_model) < self.batch_size:
-            return
-
-        minibatch = random.sample(self.memory_model, self.batch_size)
-        states = np.array([i[0] for i in minibatch])
-        actions = np.array([i[1] for i in minibatch])
-        rewards = np.array([i[2] for i in minibatch])
-        next_states = np.array([i[3] for i in minibatch])
-        dones = np.array([i[4] for i in minibatch])
+        sampled_batch = random.sample(self.memory_model, self.batch_size)
+        states = np.array([i[0] for i in sampled_batch])
+        actions = np.array([i[1] for i in sampled_batch])
+        rewards = np.array([i[2] for i in sampled_batch])
+        next_states = np.array([i[3] for i in sampled_batch])
+        dones = np.array([i[4] for i in sampled_batch])
 
         states = np.squeeze(states)
         next_states = np.squeeze(next_states)
 
-        targets = rewards + self.gamma*(np.amax(self.model.predict_on_batch(next_states), axis=1))*(1-dones)
-        targets_full = self.model.predict_on_batch(states)
-
+        q_values = self.q_learning_equation(dones, next_states, rewards)
         ind = np.array([i for i in range(self.batch_size)])
-        targets_full[[ind], [actions]] = targets
+        self.model.predict_on_batch(states)[[ind], [actions]] = q_values
 
-        self.model.fit(states, targets_full, epochs=1, verbose=0)
+        self.model.fit(states, self.model.predict_on_batch(states), epochs=1, verbose=0)
+        self.eps_decay()
+
+    def eps_decay(self):
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
+    def q_learning_equation(self, dones, next_states, rewards):
+        return rewards + self.gamma * (np.amax(self.model.predict_on_batch(next_states), axis=1)) * (1 - dones)
 
-def train_dqn(episode):
 
-    loss = []
-    agent = DeepQLearningModel(env.action_space.n, env.observation_space.shape[0])
-    for e in range(episode):
+def train_dqn(episodes_num, max_steps):
+
+    rewards = []
+    dqn_model = DeepQLearningModel(env.action_space.n, env.observation_space.shape[0])
+    for e in range(episodes_num):
         state = env.reset()
         state = np.reshape(state, (1, 4))
-        score = 0
-        max_steps = 1000
+        sum_of_rewards = 0
+
         for i in range(max_steps):
             env.render()
-            action = agent.act(state)
+            action = dqn_model.get_action(state)
             next_state, reward, done, _ = env.step(action)
-            score += reward
+            sum_of_rewards += reward
             next_state = np.reshape(next_state, (1, 4))
-            agent.remember(state, action, reward, next_state, done)
+            dqn_model.memory_model.append((state, action, reward, next_state, done))
             state = next_state
-            agent.replay()
+            dqn_model.learn()
             if done:
-                print("episode: {}/{}, score: {}".format(e, episode, score))
                 break
-        loss.append(score)
-    return loss
+        rewards.append(sum_of_rewards)
+    return rewards
 
 if __name__ == '__main__':
 
-    ep = 100
+    episodes = 100
+    max_steps = 1000
     test_ep = 10
-    loss = train_dqn(ep)
-    plt.plot([i+1 for i in range(0, ep, 2)], loss[::2])
+    reward_sum = train_dqn(episodes, max_steps)
+    plt.plot([i + 1 for i in range(0, episodes, 2)], reward_sum[::2])
     plt.show()
